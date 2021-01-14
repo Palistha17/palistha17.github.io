24818dc9293812b9af42e4bfe146d38b5468b33c
Palistha <mailpalistha1@gmail.com>
2021-01-14 13:26:55 +0545

    first commit

--- final defense/facedetect/app/src/androidTest/java/com/example/facedetect/MainActivityTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.facedetect;
-
-import static org.junit.Assert.*;
-
-public class MainActivityTest {
-
-}
--- final defense/facedetect/app/src/main/java/com/example/facedetect/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.facedetect;
-//constant such as login reg haru ko url halna
-public class Constants {
-    private static final String ROOT_URL="http://192.168.0.105/test_android/v1/";
-    public static final String URL_REGISTER=ROOT_URL+"register.php";
-    public static final String URL_Login=ROOT_URL+"userLogin.php";
-}
