cbd1047f126e6c8f94104b3ba6505de2ff7b3594
Palistha <mailpalistha1@gmail.com>
2021-01-26 08:09:26 +0545

    first commit

---
+++ jsfinalproject/js/arrow.js
@@ -0,0 +1,53 @@
+class Arrow {
+    constructor() {
+        this.isActive = false;
+        this.arrow = new Image();
+        this.arrow.src = "images/arrow.png";
+
+        this.posX = 0;
+        this.posY = 450;
+        this.speed = 5;
+    }
+
+    reset() {
+        this.posY = 450;
+        this.isActive = false;
+    }
+
+    setActive(posX) {
+        this.posX = posX;
+        this.isActive = true;
+    }
+
+    hasCollided(ball) {
+        if (!this.isActive) return false;
+
+        if (this.posX < ball.posX + ball.radius && this.posX > ball.posX - ball.radius &&
+            this.posY - ball.posY < ball.radius) {
+            this.reset();
+            return true;
+        }
+
+        return false;
+    }
+
+
+    update() {
+        if (!this.isActive) {
+            return;
+        }
+
+        this.posY -= this.speed;
+        if (this.posY == 0) {
+            this.reset();
+        }
+    }
+
+
+    draw(context) {
+        if (!this.isActive) {
+            return;
+        }
+        context.drawImage(this.arrow, this.posX, this.posY, this.arrow.width, 450);
+    }
+}
