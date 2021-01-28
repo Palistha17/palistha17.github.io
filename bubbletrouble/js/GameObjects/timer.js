da4379a2b6e128c8338a7a7b10f421993b51b853
Palistha <mailpalistha1@gmail.com>
2021-01-28 12:25:17 +0545

    new changes level1

---
+++ bubbletrouble/js/GameObjects/timer.js
@@ -0,0 +1,25 @@
+class Timer {
+    constructor(onFinish) {
+        this.seconds = 60;
+
+        this.onFinish = onFinish;
+        this.timer = setInterval(this.update, 1000);
+    }
+
+    update = () => {
+        this.seconds--;
+        if (this.seconds == 0) {
+            this.onFinish();
+        }
+    }
+
+    reset() {
+        this.seconds = 60;
+    }
+
+    draw(context) {
+        context.fillStyle = "#000";
+        context.font = "20px Verdana";
+        context.fillText("Time: " + this.seconds, 680, 60);
+    }
+}
