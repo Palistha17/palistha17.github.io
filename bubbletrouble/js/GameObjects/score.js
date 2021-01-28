0ab14298633d77471c1af193d5969eeaa9c972ac
Palistha <mailpalistha1@gmail.com>
2021-01-28 12:23:16 +0545

    new changes level1

---
+++ bubbletrouble/js/GameObjects/score.js
@@ -0,0 +1,21 @@
+class Score {
+    constructor(highscore) {
+        this.score = 0;
+        this.highscore = highscore;
+    }
+
+    update() {
+        this.score += 5;
+        if (this.score > this.highscore) {
+            this.highscore = this.score;
+        }
+    }
+
+    draw(context) {
+        context.fillStyle = "#000";
+        context.font = "20px Verdana";
+        context.fillText("Score : " + this.score, 10, 60);
+        context.fillText("Highscore : " + this.highscore, 10, 80);
+    }
+
+}
