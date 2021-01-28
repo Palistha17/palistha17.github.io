5f108beefe91e16f20778585681675151d3a3607
Palistha <mailpalistha1@gmail.com>
2021-01-28 12:20:56 +0545

    new changes level1

--- bubbletrouble/js/GameObjects/ball.js
+++ bubbletrouble/js/GameObjects/ball.js
@@ -2,6 +2,7 @@ class Ball {
     constructor(posX, posY, radius, speedX, splitCount, color) {
         this.posX = posX;
         this.posY = posY;
+
         this.color = color;
         this.radius = radius;
         this.splitCount = splitCount;
@@ -14,11 +15,11 @@ class Ball {
         this.posX += this.speedX;
         this.posY += this.speedY += 0.3;
 
-        if (this.posX > 800 - this.radius || this.posX < this.radius) {
+        if (this.posX > 800 - this.radius || this.posX < this.radius){
             this.speedX *= -1;
         }
 
-        if (this.posY > 450 - this.radius || this.posY < 0) {
+        if (this.posY > 450 - this.radius || this.posY < 0){
             this.speedY = -12;
         }
     }
@@ -27,17 +28,8 @@ class Ball {
     draw(context) {
         context.fillStyle = this.color;
         context.beginPath();
-        context.arc(
-            this.posX,
-            this.posY,
-            this.radius,
-            0,
-            Math.PI * 2,
-            false
-        );
+        context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2, false);
         context.closePath();
         context.fill();
     }
-
-
 }
