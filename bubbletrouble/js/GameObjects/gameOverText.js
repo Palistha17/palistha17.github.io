105cc8f91f50cba895032f6d6c374f155ce95481
Palistha <mailpalistha1@gmail.com>
2021-01-28 12:22:01 +0545

    new changes level1

---
+++ bubbletrouble/js/GameObjects/gameOverText.js
@@ -0,0 +1,13 @@
+class GameOverText {
+    constructor() {
+        this.text = new Image();
+        this.text.src = "images/gameover.png";
+    }
+
+    draw(context) {
+        let posX = 400 - this.text.width / 2;
+        let posY = 250 - this.text.height / 2;
+
+        context.drawImage(this.text, posX, posY);
+    }
+}
