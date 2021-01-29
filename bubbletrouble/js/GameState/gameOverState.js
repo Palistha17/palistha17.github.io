02f57e15dd5b4b08666b2a27f0656e7c00cdd3d6
Palistha <mailpalistha1@gmail.com>
2021-01-29 20:28:40 +0545

    new changes done

--- bubbletrouble/js/GameState/gameOverState.js
+++ bubbletrouble/js/GameState/gameOverState.js
@@ -3,15 +3,40 @@ class GameOverState {
         this.game = game;
 
         this.score = score;
-        this.gameOverText = new GameOverText();
-
         this.game.highscore = this.score.highscore;
+
+        this.gameOverText = new GameOverText();
+        this.menuButton = new Button('MENU', 340, 280);
+        this.playAgainButton = new Button('PLAY AGAIN', 310, 350, 160);
     }
 
-    update() { }
+    update() {
+        let mouseX = this.game.eventHandler.mouseX;
+        let mouseY = this.game.eventHandler.mouseY;
+
+        if (this.menuButton.isClicked(mouseX, mouseY)) {
+            this.showMenuState();
+        }
+
+        if (this.playAgainButton.isClicked(mouseX, mouseY)) {
+            this.showRunningState();
+        }
+    }
 
     draw() {
         this.score.draw(this.game.context);
         this.gameOverText.draw(this.game.context);
+
+        this.menuButton.draw(this.game.context);
+        this.playAgainButton.draw(this.game.context);
+    }
+
+
+    showMenuState() {
+        this.game.gameState = new MenuState(this.game)
+    }
+
+    showRunningState() {
+        this.game.gameState = new RunningState(this.game)
     }
 }
