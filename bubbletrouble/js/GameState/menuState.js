f2a10f65bc6516416a84de7c776c4b54716bccce
Palistha <mailpalistha1@gmail.com>
2021-01-29 20:29:08 +0545

    new changes done

--- bubbletrouble/js/GameState/menuState.js
+++ bubbletrouble/js/GameState/menuState.js
@@ -1,12 +1,26 @@
 class MenuState {
     constructor(game) {
         this.game = game;
-
+        this.playButton = new Button('PLAY', 460, 410);
+        this.background = new Image();
+        this.background.src = 'images/menu_bg.png';
     }
 
-    update() { }
+    update() {
+        if (this.playButton.isClicked(this.game.eventHandler.mouseX, this.game.eventHandler.mouseY)) {
+            this.showRunningState();
+        }
+    }
 
-    draw() { }
+    draw() {
+        this.game.context.drawImage(this.background, 0, 0);;
+        this.playButton.draw(this.game.context);
+
+        this.game.context.fillStyle = "#000";
+        this.game.context.font = "20px Verdana";
+        this.game.context.fillText("Use left and right arrow keys to move.", 350, 50);
+        this.game.context.fillText("Use up arrow key to shoot.", 400, 80);
+    }
 
     showRunningState() {
         this.game.gameState = new RunningState(this.game)
