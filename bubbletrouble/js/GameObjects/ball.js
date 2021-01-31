6d2fc18662c28e0f31a4b65f0b115fe7351a533f
Palistha <mailpalistha1@gmail.com>
2021-01-31 21:01:12 +0545

    new changes done

--- bubbletrouble/js/GameObjects/ball.js
+++ bubbletrouble/js/GameObjects/ball.js
@@ -9,17 +9,31 @@ class Ball {
 
         this.speedX = speedX;
         this.speedY = 0;
+
+        this.powerUp = this.selectPowerUp();
+    }
+
+    selectPowerUp() {
+        if (this.getRandomInt(10) > 5) return undefined;
+
+        let powerUpIndex = this.getRandomInt(3);
+        switch (powerUpIndex) {
+            case 0: return undefined;
+            case 1: return new Shield();
+            case 2: return new IncreaseTime();
+            default: return undefined;
+        }
     }
 
     update() {
         this.posX += this.speedX;
         this.posY += this.speedY += 0.3;
 
-        if (this.posX > 800 - this.radius || this.posX < this.radius){
+        if (this.posX > 800 - this.radius || this.posX < this.radius) {
             this.speedX *= -1;
         }
 
-        if (this.posY > 450 - this.radius || this.posY < 0){
+        if (this.posY > 450 - this.radius || this.posY < 0) {
             this.speedY = -12;
         }
     }
@@ -31,5 +45,9 @@ class Ball {
         context.arc(this.posX, this.posY, this.radius, 0, Math.PI * 2, false);
         context.closePath();
         context.fill();
+    }
+
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
 }
