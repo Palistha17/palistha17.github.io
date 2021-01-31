6c9367cbd82cc8795ebd889e0378c29ec74767fa
Palistha <mailpalistha1@gmail.com>
2021-01-31 21:10:29 +0545

    new file added

---
+++ bubbletrouble/js/PowerUps/powerUp.js
@@ -0,0 +1,53 @@
+class PowerUp {
+    constructor(imageUrl, shouldDisappear) {
+        this.posX = 0;
+        this.posY = 0;
+        this.speed = 3;
+
+        this.image = new Image();
+        this.image.src = imageUrl;
+
+        this.isDropped = false;
+        this.isActive = false;
+        this.isExecuted = false;
+        this.shouldDisappear = shouldDisappear;
+    }
+
+    drop(posX, posY) {
+        this.posX = posX;
+        this.posY = posY;
+        this.isDropped = true;
+    }
+
+    setActive(posX) {
+        this.posX = posX;
+        this.isDropped = false;
+
+        if (this.isActive || this.isExecuted) return;
+
+        this.posY = 450 - this.image.height;
+        this.isActive = true;
+    }
+
+    setInactive() {
+        this.isActive = false;
+    }
+
+    setExecuted() {
+        this.isExecuted = true;
+    }
+
+    hasCollided(player) {
+        return this.posX < player.posX + player.width && this.posX + 20 > player.posX && this.posY + 20 > player.posY;
+    }
+
+    update() {
+        if (!this.isDropped) return;
+        if (this.posY < 450 - 20) this.posY += this.speed;
+    }
+
+    draw(context) {
+        if (this.isActive && !this.shouldDisappear) context.drawImage(this.image, this.posX, this.posY);
+        else if (this.isDropped) context.drawImage(this.image, this.posX, this.posY, 20, 20);
+    }
+}
