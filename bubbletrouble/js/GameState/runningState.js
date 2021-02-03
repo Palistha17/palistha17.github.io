9efda604914d7b2c156c3d0d46126f5367ca010b
Palistha <mailpalistha1@gmail.com>
2021-02-03 11:34:48 +0545

    new changes done

--- bubbletrouble/js/GameState/runningState.js
+++ bubbletrouble/js/GameState/runningState.js
@@ -4,74 +4,76 @@ class RunningState {
 
         this.timer = new Timer(() => this.showGameOverState());
         this.score = new Score(this.game.highscore);
-
         this.player = new Player();
-        this.addBalls();
 
         this.activateShield = false;
-    }
-
-    addBalls() {
-        this.balls = [];
-        this.balls.push(new Ball(200, 200, 25, 3, 3, '#FF0000'));
-        this.balls.push(new Ball(400, 200, 30, 3, 2, '#00FF00'));
     }
 
     update() {
         this.detectCollisions();
         this.player.update();
-        for (let ball of this.balls) ball.update();
     }
 
     draw() {
         this.player.draw(this.game.context);
         this.score.draw(this.game.context);
         this.timer.draw(this.game.context);
-        for (let ball of this.balls) ball.draw(this.game.context);
     }
 
     detectCollisions() {
         for (let powerUp of this.player.powerUps) {
-            if (powerUp.hasCollided(this.player)) powerUp.execute(this);
+            if (powerUp.hasCollided(this.player)) {
+                powerUp.setActive(this.player.posX);
+                powerUp.execute(this);
+            }
         }
+    }
 
-        for (let ball of this.balls) {
+    detectBallCollisions(state) {
+        for (let ball of state.balls) {
             if (this.player.arrow.hasCollided(ball)) {
-                if (ball.powerUp) this.player.addPowerUp(ball.powerUp, ball.posX, ball.posY);
+                if (ball.powerUp) {
+                    this.player.addPowerUp(ball.powerUp, ball.posX, ball.posY);
+                }
 
-                this.splitBall(ball);
+                this.splitBall(ball, state.balls);
                 this.score.update();
             }
 
             if (!this.activateShield) {
                 if (this.player.hasCollided(ball)) {
-                    this.player.lives--;
-                    if (this.player.lives > 0) {
-                        this.addBalls();
-                        this.timer.reset();
-                        this.player.reset();
-                    } else {
-                        this.showGameOverState();
-                    }
+                    this.resetLevel(state);
                 }
             }
         }
     }
 
+    resetLevel(state) {
+        this.player.lives--;
+        if (this.player.lives > 0) {
+            state.addBalls();
+            this.player.reset();
+            this.timer.reset();
+        } else {
+            this.showGameOverState();
+        }
+    }
 
-    splitBall(ball) {
+
+    splitBall(ball, balls) {
         if (ball.splitCount > 0) {
-            var b1 = new Ball(ball.posX, ball.posY, ball.radius / 1.7, ball.speedX, ball.splitCount - 1, ball.color);
-            var b2 = new Ball(ball.posX, ball.posY, ball.radius / 1.7, -ball.speedX, ball.splitCount - 1, ball.color);
-            this.balls.push(b1);
-            this.balls.push(b2);
+            var b1 = new Ball(ball.posX, ball.posY, ball.radius / 1.7, ball.speedX, ball.splitCount - 1, ball.color, ball.bounceHeight);
+            var b2 = new Ball(ball.posX, ball.posY, ball.radius / 1.7, -ball.speedX, ball.splitCount - 1, ball.color, ball.bounceHeight);
+            balls.push(b1);
+            balls.push(b2);
         }
 
-        let ballIndex = this.balls.indexOf(ball);
-        this.balls.splice(ballIndex, 1);
+        let ballIndex = balls.indexOf(ball);
+        balls.splice(ballIndex, 1);
     }
 
     showGameOverState() {
+        clearInterval(this.timer.timer);
         this.game.gameState = new GameOverState(this.game, this.score)
     }
 }
