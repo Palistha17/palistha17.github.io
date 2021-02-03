79066d0ef73f6c8b230f31e8df3772eb63b154e5
Palistha <mailpalistha1@gmail.com>
2021-02-03 11:36:57 +0545

    new file added

---
+++ bubbletrouble/js/GameState/level2State.js
@@ -0,0 +1,46 @@
+class Level2State extends RunningState {
+    constructor(game) {
+        super(game);
+        this.game = game;
+
+        this.addBalls();
+        this.spikes = new Spikes(true, 250, 250);
+
+        this.background = new Image();
+        this.background.src = "images/level2.jpg";
+
+    }
+
+    addBalls() {
+        this.balls = [];
+        this.balls.push(new Ball(100, 350, 10, 3, 0, '#00FF00', 10));
+    }
+
+    update() {
+        if (this.balls.length == 0) {
+            this.showNextLevel();
+        }
+
+        super.update();
+        this.detectBallCollisions(this);
+        for (let ball of this.balls) ball.update();
+    }
+
+    draw() {
+        this.game.context.drawImage(this.background, 0, 0, 800, 450);
+        this.game.context.fillStyle = "#000";
+        this.game.context.font = "20px Verdana";
+        this.game.context.fillText("level 2", 700, 480);
+
+        super.draw();
+        this.spikes.draw(this.game.context);
+        for (let ball of this.balls) {
+            ball.draw(this.game.context);
+        }
+    }
+
+    showNextLevel() {
+        clearInterval(this.timer.timer);
+        this.game.gameState = new Level3State(this.game);
+    }
+}
