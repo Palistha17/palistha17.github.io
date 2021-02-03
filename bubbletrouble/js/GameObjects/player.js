a9a60d20575c53aaf666e146dc3ade34fa6835d8
Palistha <mailpalistha1@gmail.com>
2021-02-03 11:23:24 +0545

    new changes done

--- bubbletrouble/js/GameObjects/player.js
+++ bubbletrouble/js/GameObjects/player.js
@@ -1,110 +1,113 @@
+const DIR_RIGHT = 0;
+const DIR_LEFT = 1;
+const DIR_STRAIGHT = 2;
+
 class Player {
-  constructor() {
-      this.posX = 400;
-      this.posY = 394;
+    constructor() {
+        this.posX = 400;
+        this.posY = 394;
 
-      this.width = 46;
-      this.height = 56;
+        this.width = 46;
+        this.height = 56;
 
-      this.lives = 3;
+        this.lives = 3;
 
-      this.arrow = new Arrow();
-      this.player = new Image();
-      this.player.src = "images/player.png";
-      this.powerUps = [];
+        this.arrow = new Arrow();
+        this.player = new Image();
+        this.player.src = "images/player.png";
+        this.powerUps = [];
+        this.dir = DIR_STRAIGHT;
 
-      document.addEventListener("keydown", this.handlePosition);
-      this.context = document.getElementById("canvas").getContext("2d");
-  }
+        document.addEventListener("keydown", this.handlePosition);
+        document.addEventListener("keyup", this.resetPosition);
+    }
 
- handlePosition = (event) => {
-      event.preventDefault();
-      if (event.code == "ArrowLeft") {
-          this.posX -= 10;
-          if (this.posX < this.width) {
-            this.posX = 0;  
-          } 
-          this.context.drawImage(this.player, 0, 56, this.width, this.height, this.posX, this.posY, this.width, this.height);
-      } else if (event.code == "ArrowRight") {
-          this.posX += 10;
-          if (this.posX > 800 - this.width) {
-            this.posX = 800 - this.width;  
-          } 
-          this.context.drawImage(this.player, 0, 0, this.width, this.height, this.posX, this.posY, this.width, this.height);
-      } else if (event.code == "ArrowUp") {
-          if (!this.arrow.isActive) {
-              this.arrow.setActive(this.posX);
-          }
-      }
+    handlePosition = (event) => {
+        event.preventDefault();
+        if (event.code == "ArrowLeft") {
+            this.posX -= 10;
+            if (this.posX < this.width) {
+                this.posX = 0;
+            }
+            this.dir = DIR_LEFT;
+        } else if (event.code == "ArrowRight") {
+            this.posX += 10;
+            if (this.posX > 800 - this.width) {
+                this.posX = 800 - this.width;
+            }
+            this.dir = DIR_RIGHT;
+        } else if (event.code == "ArrowUp") {
+            if (!this.arrow.isActive) {
+                this.arrow.setActive(this.posX);
+            }
+            this.dir = DIR_STRAIGHT;
+        }
+    }
 
-  }
-  
-  addPowerUp(powerUp, posX, posY) {
-      powerUp.drop(posX, posY);
-      this.powerUps.push(powerUp);
-  }
+    resetPosition = () => {
+        this.dir = DIR_STRAIGHT;
+    }
+
+    addPowerUp(powerUp, posX, posY) {
+        powerUp.drop(posX, posY);
+        this.powerUps.push(powerUp);
+    }
 
 
-  reset() {
-      this.powerUps = [];
-      this.posX = 400;
-  }
+    reset() {
+        this.powerUps = [];
+        this.posX = 400;
+    }
 
-  hasCollided(ball) {
-      let ballDistanceX = Math.abs(ball.posX - this.posX);
-      let ballDistanceY = Math.abs(ball.posY - this.posY);
+    hasCollided(ball) {
+        let ballDistanceX = Math.abs(ball.posX - this.posX);
+        let ballDistanceY = Math.abs(ball.posY - this.posY);
 
-      if (ballDistanceX > (this.width / 2 + ball.radius)) {
-          return false;
-      }
-      if (ballDistanceY > (this.height / 2 + ball.radius)) {
-          return false;
-      }
+        if (ballDistanceX > (this.width / 2 + ball.radius)) {
+            return false;
+        }
+        if (ballDistanceY > (this.height / 2 + ball.radius)) {
+            return false;
+        }
 
-      if (ballDistanceX <= (this.width / 2)) {
-          return true;
-      }
-      if (ballDistanceY <= (this.height / 2)) {
-          return true;
-      }
+        if (ballDistanceX <= (this.width / 2)) {
+            return true;
+        }
+        if (ballDistanceY <= (this.height / 2)) {
+            return true;
+        }
 
-      let cornerDistanceSq = Math.pow((ballDistanceX - this.width / 2), 2) +
-          Math.pow((ballDistanceY - this.height / 2), 2);
+        let cornerDistanceSq = Math.pow((ballDistanceX - this.width / 2), 2) +
+            Math.pow((ballDistanceY - this.height / 2), 2);
 
-      return (cornerDistanceSq <= Math.pow(ball.radius, 2));
+        return (cornerDistanceSq <= Math.pow(ball.radius, 2));
 
-  }
+    }
 
-  update() {
-      this.arrow.update();
+    update() {
+        this.arrow.update();
 
-      if (this.powerUps.length > 0) {
-          for (let powerUp of this.powerUps) {
-              if (powerUp.isExecuted) {
-                  let powerUpIndex = this.powerUps.indexOf(powerUp);
-                  this.powerUps.splice(powerUpIndex, 1);
-                  continue;
-              }
+        if (this.powerUps.length == 0) return;
 
-              powerUp.update();
+        for (let powerUp of this.powerUps) {
+            powerUp.update();
+            if (powerUp.isExecuted) {
+                let powerUpIndex = this.powerUps.indexOf(powerUp);
+                this.powerUps.splice(powerUpIndex, 1);
+                continue;
+            }
+        }
+    }
 
-              if (powerUp.hasCollided(this)) {
-                  powerUp.setActive(this.posX);
-              }
-          }
-      }
-  }
+    draw(context) {
+        this.arrow.draw(context);
+        context.drawImage(this.player, 0, 56 * this.dir, this.width, this.height, this.posX, this.posY, this.width, this.height);
 
-  draw(context) {
-      this.arrow.draw(context);
-      context.drawImage(this.player, 0, 112, this.width, this.height, this.posX, this.posY, this.width, this.height);
+        context.fillStyle = "#000";
+        context.font = "20px Verdana";
+        context.fillText("Lives : " + this.lives, 10, 480);
 
-      context.fillStyle = "#000";
-      context.font = "20px Verdana";
-      context.fillText("Lives : " + this.lives, 10, 480);
-
-      if (this.powerUps.length > 0) {
-          for (let powerUp of this.powerUps) powerUp.draw(context);
-      }
-  }
+        if (this.powerUps.length == 0) return;
+        for (let powerUp of this.powerUps) powerUp.draw(context);
+    }
 }
