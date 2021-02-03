1e061a01c54f60d14fc31e343b7ece42641ef7e6
Palistha <mailpalistha1@gmail.com>
2021-02-03 11:24:17 +0545

    new changes done

--- bubbletrouble/js/GameObjects/spikes.js
+++ bubbletrouble/js/GameObjects/spikes.js
@@ -1,16 +1,36 @@
 class Spikes {
-    constructor() {
+    constructor(shouldMove, posY = 10, ceilingHeight = 10) {
         this.spikes = new Image();
         this.spikes.src = "images/spikes.png";
 
         this.ceiling = new Image();
         this.ceiling.src = "images/floor.jpg";
+
+        this.posY = posY;
+        this.shouldMove = shouldMove;
+        this.ceilingHeight = ceilingHeight;
+
+    }
+
+    reset() {
+        this.posY = 10;
+    }
+
+    update() {
+        if (!this.shouldMove) return;
+        this.posY += 0.5;
+    }
+
+    hasCollided(player) {
+        return this.posY + 10 > player.posY;
     }
 
     draw(context) {
         for (let i = 0; i <= 20; i++) {
-            context.drawImage(this.spikes, i * 40, 10, this.spikes.width, 10);
+            context.drawImage(this.spikes, i * 40, this.posY, this.spikes.width, 10);
         }
-        context.drawImage(this.ceiling, 0, 0, 800, 10);
+        context.drawImage(this.ceiling, 0, 0, 800, this.ceilingHeight);
     }
+
+
 }
