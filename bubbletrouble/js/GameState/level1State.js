eadc36fd21118d4c7ddf6dc4dd9267f06edde896
Palistha <mailpalistha1@gmail.com>
2021-02-04 18:15:08 +0545

    final commit

--- bubbletrouble/js/GameState/level1State.js
+++ bubbletrouble/js/GameState/level1State.js
@@ -10,10 +10,10 @@ class Level1State extends RunningState {
         this.background.src = "images/level1.jpg";
     }
 
-
     addBalls() {
         this.balls = [];
-        this.balls.push(new Ball(400, 200, 30, 3, 1, '#00FF00'));
+        this.balls.push(new Ball(400, 200, 30, 3, 2, '#00FF00', 12));
+        this.balls.push(new Ball(100, 200, 20, 3, 0, '#FF0000', 12));
     }
 
     update() {
@@ -30,7 +30,7 @@ class Level1State extends RunningState {
         this.game.context.drawImage(this.background, 0, 0, 800, 450);
         this.game.context.fillStyle = "#000";
         this.game.context.font = "20px Verdana";
-        this.game.context.fillText("Level 1", 700, 480);
+        this.game.context.fillText("Level 1", 350, 480);
 
         super.draw();
         this.spikes.draw(this.game.context);
